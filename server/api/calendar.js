5877cebce65ef873914895bc8f9b1dca35276cdf
Anatoli Nicolae <desk@anatolinicolae.com>
2022-04-03 00:57:19 +0200

    Solve coding challenge
    Signed-off-by: Anatoli Nicolae <desk@anatolinicolae.com>

--- server/api/calendar.js
+++ server/api/calendar.js
@@ -1,60 +1,57 @@
+const db = require('db');
+const Moment = require('moment');
+const MomentRange = require('moment-range');
+const moment = MomentRange.extendMoment(Moment);
 const express = require('express');
 const router = express.Router();
 
-router.get('/api/calendar', (req, res) => {
+router.get('/api/calendar', async (req, res) => {
   const { hostUserId } = req.query;
+  const timeslotLengthMin = 60;
+
+  // Get events from DB
+  const events = await db.calendar.findEventsForUser(hostUserId);
+  // Convert ranges into moment ranges, to avoid doing this every time in future loops
+  const eventsRanges = events.map(e => moment.range(e.start, e.end));
+
+  // Get current day and future 7-day range from tomorrow
+  const today = moment().startOf('day');
+  const start = today.clone().add(1, 'days');
+  const end = today.clone().add(7, 'days');
+
+  // Looop through events and check for overlapping ranges
+  const matchEventsToSlot = startAt => {
+    const endAt = startAt.clone().add(timeslotLengthMin, 'minutes');
+    const slot = moment.range(startAt, endAt);
+
+    // Search for an overlapping range, not finding one means the slot is available
+    if (eventsRanges.find(eventRange => slot.overlaps(eventRange)) === undefined) {
+      return startAt.format('YYYY-MM-DDTHH:mm:ss.SSS');
+    }
+
+    return false;
+  };
+
+  // Fetch slots for a single day, starting at 9am ending at 4pm
+  const fetchSlotsByDay = day => {
+    const startingHour = day.clone().set('hour', 9);
+    const endingHour = day.clone().set('hour', 16);
+
+    // Get available slots during the day, by specified time slot length
+    const slots = Array.from(moment.range(startingHour, endingHour).by('minutes', { step: timeslotLengthMin }));
+
+    // Filter out slots with overlapping events
+    return slots.filter(startAt => matchEventsToSlot(startAt));
+  }
+
+  // Fetch slots looping through days, then flatten the array
+  const timeslots = Array.from(moment.range(start, end).by('days')).map(day => fetchSlotsByDay(day)).flat();
 
   res.json({
     name: 'Eng Test User',
-    timeslotLengthMin: 60,
-    // This is mock data that you should remove and replace with `db.calendar.findEventsForUser`.
-    // See the README for more details.
-    timeslots: [
-      '2021-11-24T14:00:00.000',
-      '2021-11-24T15:00:00.000',
-      '2021-11-24T16:00:00.000',
-      '2021-11-24T19:00:00.000',
-      '2021-11-24T20:00:00.000',
-      '2021-11-24T21:00:00.000',
-      '2021-11-25T14:00:00.000',
-      '2021-11-25T18:00:00.000',
-      '2021-11-25T19:00:00.000',
-      '2021-11-25T20:00:00.000',
-      '2021-11-25T21:00:00.000',
-      '2021-11-26T14:00:00.000',
-      '2021-11-26T15:00:00.000',
-      '2021-11-26T16:00:00.000',
-      '2021-11-26T19:00:00.000',
-      '2021-11-26T20:00:00.000',
-      '2021-11-26T21:00:00.000',
-      '2021-11-27T14:00:00.000',
-      '2021-11-27T15:00:00.000',
-      '2021-11-27T16:00:00.000',
-      '2021-11-27T17:00:00.000',
-      '2021-11-27T18:00:00.000',
-      '2021-11-27T19:00:00.000',
-      '2021-11-27T20:00:00.000',
-      '2021-11-27T21:00:00.000',
-      '2021-11-28T14:00:00.000',
-      '2021-11-28T20:00:00.000',
-      '2021-11-28T21:00:00.000',
-      '2021-11-29T14:00:00.000',
-      '2021-11-29T15:00:00.000',
-      '2021-11-29T16:00:00.000',
-      '2021-11-29T17:00:00.000',
-      '2021-11-29T18:00:00.000',
-      '2021-11-29T19:00:00.000',
-      '2021-11-29T20:00:00.000',
-      '2021-11-29T21:00:00.000',
-      '2021-11-30T14:00:00.000',
-      '2021-11-30T15:00:00.000',
-      '2021-11-30T16:00:00.000',
-      '2021-11-30T17:00:00.000',
-      '2021-11-30T18:00:00.000',
-      '2021-11-30T19:00:00.000',
-      '2021-11-30T20:00:00.000',
-      '2021-11-30T21:00:00.000',
-    ],
+    timeslotLengthMin,
+    events,
+    timeslots,
   });
 });
 
